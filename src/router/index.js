46acaa0b9743b65a43da7fc2bd6df9bbd811bde6
Mbida Daniel <daniel.mbida@he-arc.ch>
2026-02-07 13:23:49 +0100

    Configure the project structure by adding employee management and routing pages.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import EmployeeListPage from '@/views/EmployeeListPage.vue'
+import EmployeeDetailPage from '@/views/EmployeeDetailPage.vue'
+import CreateEmployeePage from '@/views/CreateEmployeePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: 'employee/list', component: EmployeeListPage },
+    { path: '/employee/:id', name: 'employeeDetail', component: EmployeeDetailPage, props: true },
+    { path: '/employee/create', name: 'createEmployee', component: CreateEmployeePage },
+  ],
 })
-
 export default router
---
+++ src/views/EmployeeDetailPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Employee detail</h1>
+  </div>
+</template>
